cdfd726de04367b8161d8f92224c5fd0a5720d08
Abdullah Al Faroque <rfnshare@gmail.com>
2023-11-18 15:54:04 +0600

    Dynamic Routing

--- books/views.py
+++ books/views.py
@@ -1,7 +1,20 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import json
+
+booksData = open('C:\\Users\\abdullah.faroque\\PycharmProjects\\bitfumes\\bookstore\\books.json').read()
+data = json.loads(booksData)
 
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello There")
+    context = {'books': data}
+    return render(request, 'books/index.html', context)
+
+
+def show(request, id):
+    global da
+    for book in data:
+        if book['id'] == id:
+            da = book
+    return render(request, 'books/show.html', da)
